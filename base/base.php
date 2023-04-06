86227a466995fc13dbba0ecf23ea08430cfa2fbd
Andrecosta86 <andrelf.cost@gmail.com>
2023-04-05 21:57:37 -0300

    Formulario pessoas MySQL

---
+++ base/base.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <color:red><title>BOAS VINDAS</title></color:red>
+
+    </head>
+    <body>
+        <h1>Exercício 06 - Interação entre FROM e PHP</h1><br>
+        <p>Curso PHP da instituição Senai</p><br>
+      <?php 
+      
+      
+      ?>  
+
+     
+    </body>
+    
+ 
+</html>
+
+
---
+++ crud-mysql/form-pessoas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <color:red><title>Form Pessoas</title></color:red>
+
+    </head>
+    <body>
+        <h1><color:red>Formulario Pessoas</h1></color:red><br>
+        <text>Curso PHP da instituição Senai</text><br>
+        
+      <?php 
+      
+      
+      ?>  
+
+     
+    </body>
+    
+ 
+</html>
+
