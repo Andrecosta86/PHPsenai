8d66ade77e8d324e8d99a2dc25f9e5954d1e30e6
Andrecosta86 <andrelf.cost@gmail.com>
2023-04-09 17:40:00 -0300

    Exercício 08 - Manutenção Evolutiva

--- crud-mysql/conectar.php
+++ crud-mysql/conectar.php
@@ -2,17 +2,20 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <color:red><title>Banco de Dados</title></color:red>
+        <title>Banco de Dados</title>
 
     </head>
     <body>
-        <h1>conexão com o banco de dados Mysql</h1><br>
-        <p>Curso PHP da instituição Senai</p><br>
-      <?php 
+       
+      <?php
+       // criando conexão com o servidor
+// configuração para o servidor MySQL essa função retorna uma variável que tem acesso ao BD
+function CONECTAR()
+    {
       $servidor ="localhost";
       $usuario ="root";
       $senha ="";
-// criando conexão com o servidor
+
       $con = new mysqli($servidor, $usuario, $senha, "myDB");
 // teste de conexão com o servidor
 
@@ -20,17 +23,57 @@
         die ("connection failed: " . $con->connect_error);
 
     }
-    echo "Connected successfully<br>"; 
-
-    function incluir($nome, $email, $cpf){
-        $sql = "insert into pessoas(nome, email, cpf) values ('$nome', '$email' ,'$cpf')";
+    echo "Connected successfully<br>";
+    return $con; 
+    }
+    //Função para incluir uma nova pessoa no banco de dados
+    function INCLUIR($nome, $email, $cpf){
+        $con = CONECTAR();
+        $sql = "insert into pessoas(nome, email, cpf) values ('$nome', '$email','$cpf')";
         if( $con->query($sql)===true){
             return "ok ao gravar";
-        }
-        else{
-            return "Erro: $sql" . $con->error;
+        }else{
+            return "Erro:$sql" .$con->error;
+             }   
+    }
+//Buscar todas as pessoas que estão gravadas no no myDB
+    function LISTAR(){
+        $con = CONECTAR();
+        $sql = "select id, nome, email, cpf from pessoas";
+        $resultado = $con->query($sql);
+        return $resultado;
+
+    }
+
+    function BUSCAR($id){
+        $con = CONECTAR();
+        $sql = "select id, nome, cpf from pessoas where id = $id";
+        $resultado = $con ->query($sql);
+        $resultado =$resultado->fetch_assoc();
+        return $resultado;
+        
+    }
+
+    function ALTERAR($id, $nome, $email, $cpf){
+        $con = conectar();
+        $sql = "UPDATE pessoas SET nome = '$nome', email = '$email', cpf ='$cpf' where id = $id";
+        if($con->query($sql) === true){
+            return "Ok ao Atualizar";
+        }else{
+            return "Erro: $sql".$con->error;
         }
     }
+    
+    function APAGAR($id){
+        $con = conectar();
+        $sql = "DELETE from pessoas where id = $id";
+        if($con->query($sql) === true){
+            return "Ok ao Apagar";
+        }else{
+            return "Erro: $sql".$con->error;
+        }
+    }
+
 
       ?>  
 
--- crud-mysql/form-pessoas.php
+++ crud-mysql/form-pessoas.php
@@ -1,22 +1,121 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <color:red><title>Form Pessoas</title></color:red>
-
+        <link rel="stylesheet" type="text/css" href="custumer.css">
+        <title>Form Pessoas</title>
+        
     </head>
     <body>
-        <h1><color:red>Formulario Pessoas</h1></color:red><br>
-        <text>Curso PHP da instituição Senai</text><br>
-        
-      <?php 
-      
-      
-      ?>  
-
-     
-    </body>
+     <fieldset>  
+    <div>
+        <h1>Formulario Pessoas</h1><br>
+        <div>
+         <img src="https://static.portaldaindustria.com.br/static/img/logos/azul/SENAI-home.svg" 
+        alt="Logo Senai"  height="50" width="200"/>   
+                   
+        </div>
+        <text><strong>Curso PHP da instituição Senai</strong></text><br>
+        <text><strong>Preencha os dasdos abaixo para efeituar o cadastro:</strong></text><br>
+    </div>
+     <?php
+    include 'conectar.php';
     
+   $id = $nome = $email = $cpf ="";
+     if($_SERVER["REQUEST_METHOD"]=="GET"){
+        if(array_key_exists("id",$_GET)){
+        $id = $_GET["id"];
+        $pessoas = BUSCAR($id);
+        $nome = $pessoas['nome'];
+        $email = $pessoas['email'];
+        $cpf = $pessoas['cpf'];
+        }  
+     }
+     ?>
+  
+    <form action="form-pessoas.php" method="post">
+
+       
+            <strong>Nome:</strong><br>
+            <input type="text" name="nome"value=" <?php echo $nome;?>"required><br>
+            <strong>E-mail:</strong><br>
+            <input type="email" name="email"value="<?php echo $email?>"required><br>
+            <strong>CPF:</strong><br>
+            <input type="text" name="cpf"value="<?php echo $cpf?>"required><br><br>
+            <button class=botao type= submit> GRAVAR</button>
+            <!-- Botão para um chamar a pagina com campos limpos -->
+            <a href="form-pessoas.php"><input type= "button" value ="NOVO"> </a>
+            <!-- Botão para apagar um dado somedo do id selecionado-->
+            <?php echo 'onclick="window.location.replace(\'form-pessoa.php?apagar="';
+    echo '$id\')"'; ?>
+           
+              
+
+
+              
+            
+</fieldset>
+             <!--  Genero:<br>
+             <input type="radio" name="genero" required value ="masculino">Masculino
+            <br><input type="radio" name="genero" required value ="feminino">Feminino
+            <br><input type="radio" name="genero" required value ="outros">Outros
+           
+         <select sexo="Genero:<br>"required>
+                <option selected disabled value="">Escolha</option>
+                <option>Masculino</option>
+                <option>Feminino</option>
+                <option>Outros</option>
+            </select><br><br>-->
+            
+    <fieldset class="tabela">   
+    </form>
+     
+        <?php  
+       
+        if ($_SERVER["REQUEST_METHOD"] == "POST")
+        { 
+        $id = $_POST['id'];
+        $nome = $_POST['nome'];
+        $email =$_POST['email'];
+        $cpf =$_POST['cpf'];
+       
+       // $genero=$_POST['genero'];
+
+       if($id==''){
+                    $msg =INCLUIR($nome, $email, $cpf);
+                 } else{
+                     $msg = ALTERAR($id, $nome, $email, $cpf);
+                 }
+        }echo"<br>";
+        ?>  
+    <table border "0.5">
+    <tr>
+      <th>ID</th>  
+      <th>Nome</th> 
+      <th>E-mail</th> 
+      <th>CPF</th> 
+      <!--<th>Genero</th>-->
+    </tr>
+<?php
+$dados = LISTAR();
+while($linha =$dados->fetch_assoc())
+{
+   
+echo "<tr>";
+echo "<td>" .$linha ['id']. "</td>";
+echo "<td>" .$linha ['nome']. "</td>";
+echo "<td>" .$linha ['email']. "</td>";
+echo "<td>" .$linha ['cpf']. "</td>";
+//echo "<td>" .$linha ['genero']. "</td>";
+echo "<td><a href='form-pessoas.php?id=".$linha ['id']."'>Editar</a></td>";
+echo "<td><a href='form-pessoa.php?apagar=".$linha['id']."'>Apagar</a></td>";
+echo "</tr>";
+}
+?>
+    </table>
+</fieldset>      
+</body>
+   
  
 </html>
 
